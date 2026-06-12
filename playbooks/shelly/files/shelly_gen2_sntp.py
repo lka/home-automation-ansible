c3630f67df64d0f745db4f9fa6a50e5d987eacc1
Lka <3001100+lka@users.noreply.github.com>
2026-06-12 17:01:16 +0200

    Gen2/3 SNTP nur setzen wenn Änderung nötig

--- playbooks/shelly/files/shelly_gen2_sntp.py
+++ playbooks/shelly/files/shelly_gen2_sntp.py
@@ -9,10 +9,31 @@ user = sys.argv[2]
 password = sys.argv[3]
 ntp_server = sys.argv[4]
 
+auth = HTTPDigestAuth(user, password)
+
+# Aktuelle Konfiguration lesen
+r = requests.post(
+    f'http://{host}/rpc',
+    json={'id':1,'method':'Sys.GetConfig','params':{}},
+    auth=auth,
+    timeout=10
+)
+current = r.json()
+current_server = current.get('result', {}).get('sntp', {}).get('server', '')
+
+if current_server == ntp_server:
+    print(json.dumps({'changed': False, 'msg': f'Timeserver bereits korrekt: {current_server}'}))
+    sys.exit(0)
+
+# Nur setzen wenn abweichend
 r = requests.post(
     f'http://{host}/rpc',
     json={'id':1,'method':'Sys.SetConfig','params':{'config':{'sntp':{'server':ntp_server}}}},
-    auth=HTTPDigestAuth(user, password),
+    auth=auth,
     timeout=10
 )
-print(r.text)
+result = r.json()
+result['changed'] = True
+result['old_server'] = current_server
+result['new_server'] = ntp_server
+print(json.dumps(result))
